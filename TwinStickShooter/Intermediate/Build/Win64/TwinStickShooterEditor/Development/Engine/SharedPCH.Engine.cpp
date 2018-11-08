376cc6294bdb92e175063da35cacfef8347a127c
ADS\nsshipma <nsshipma@gmail.com>
2018-11-08 16:23:50 -0500

    Settings updated and Player Character blueprint set up

--- TwinStickShooter/Intermediate/Build/Win64/TwinStickShooterEditor/Development/Engine/SharedPCH.Engine.cpp
+++ TwinStickShooter/Intermediate/Build/Win64/TwinStickShooterEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/Nate/Documents/GitHub/nsshipma_FinalProject/TwinStickShooter/Intermediate/Build/Win64/TwinStickShooterEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/nsshipma.ADS.005/Documents/nsshipma_FinalProject/TwinStickShooter/Intermediate/Build/Win64/TwinStickShooterEditor/Development/Engine/SharedPCH.Engine.h"
